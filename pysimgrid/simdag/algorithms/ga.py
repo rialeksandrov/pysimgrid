392a753dafcf69530275262270799b41bf62d1e3
Роман <rialeksandrov@gmail.com>
2017-04-14 23:03:12 +0300

    Working version of Genetic Algorithm

--- pysimgrid/simdag/algorithms/ga.py
+++ pysimgrid/simdag/algorithms/ga.py
@@ -69,21 +69,48 @@ class Chromosome:
   def copy_schedule(self, schedule_and_span):
     self.matching = {}
     self.scheduling = {}
-    id = 0
+
     for host in self.hosts:
       for task in schedule_and_span[0][host]:
-        self.matching[task] = host
-        self.scheduling[task] = id
-        id = id + 1
+        if task.name != "root" or task.name != "end":
+          self.matching[task] = host
+
+    indexes = {host : 0 for host in self.hosts}
+    undone_parents = {task: len(self.nxgraph.pred[task]) for task in self.tasks}
+    for task in self.tasks:
+      for parent in self.nxgraph.pred[task]:
+        if parent.name == "root":
+          undone_parents[task] -= 1
+          break
+
+    for i in range(len(self.tasks)):
+      task = self.tasks[0]
+      ok = 0
+      for host in self.hosts:
+        if indexes[host] == len(schedule_and_span[0][host]):
+          continue
+        if undone_parents[schedule_and_span[0][host][indexes[host]]] == 0:
+          task = schedule_and_span[0][host][indexes[host]]
+          indexes[host] += 1
+          ok = 1
+          break
+      assert ok == 1, "Did not find task to schedule!"
+      self.scheduling[task] = i
+      for child, edge in self.nxgraph[task].items():
+        if child.name != "end":
+          undone_parents[child] -= 1
+
 
   def scheduling_mutation(self):
     task = random.choice(self.tasks)
     left_boundary = 0
     right_boundary = len(self.tasks) - 1
     for child, edge in self.nxgraph[task].items():
-      right_boundary = min(right_boundary, self.scheduling[child] - 1)
+      if child.name != "end":
+        right_boundary = min(right_boundary, self.scheduling[child] - 1)
     for parent in self.nxgraph.pred[task]:
-      left_boundary = max(left_boundary, self.scheduling[parent] + 1)
+      if parent.name != "root":
+        left_boundary = max(left_boundary, self.scheduling[parent] + 1)
 
     new_id = random.randint(left_boundary, right_boundary)
     old_id = self.scheduling[task]
@@ -98,9 +125,11 @@ class Chromosome:
 
     self.scheduling[task] = new_id
 
+
   def matching_mutation(self):
     task = random.choice(self.tasks)
     self.matching[task] = random.choice(self.hosts)
+
 
   def make_task_order(self):
     res = copy.copy(self.tasks)
@@ -108,15 +137,15 @@ class Chromosome:
       res[self.scheduling[task]] = task
     return res
 
+
   def matching_crossover(self, gen):
     cut_off = random.randint(0, len(self.tasks) - 1)
-    print(cut_off)
     for task in self.tasks:
       if cut_off == 0:
         gen.matching[task], self.matching[task] = self.matching[task], gen.matching[task]
       else:
         cut_off -= 1
-    #print (gen.matching)
+
 
   def scheduling_crossover(self, gen):
     cut_off = random.randint(0, len(self.tasks) - 1)
@@ -147,6 +176,7 @@ class Chromosome:
     gen.scheduling = first_res
     self.scheduling = second_res
 
+
   def get_schedule_by_name(self):
     schedule = {host.name: [] for host in self.hosts}
     order = self.make_task_order()
@@ -154,9 +184,13 @@ class Chromosome:
       task = order[i]
       schedule[self.matching[task].name].append(task.name)
       # print("host :", self.matching[task].name, " task: ", task.name)
+    # Add important:
+    schedule["master"] = ["root", "end"]
     return schedule
 
+
   def get_schedule(self):
+    assert False, "Deprecated"
     schedule = {host: [] for host in self.hosts}
     order = self.make_task_order()
     for i in range(len(order)):
@@ -165,10 +199,9 @@ class Chromosome:
       # print("host :", self.matching[task].name, " task: ", task.name)
     return schedule
 
+
   def __eq__(self, other):
     return self.scheduling == other.scheduling and self.matching == other.matching
-
-
 
 
 
@@ -188,6 +221,7 @@ def _restore_schedule(simulation, serialized):
       host_schedule.append(tasks["end"])
       break
   return result, end_scheduled
+
 
 def _evaluation(platform_path, tasks_path, schedule_by_name):
   import logging
@@ -213,6 +247,7 @@ def _serialize_graph(graph, output_file):
   output_file.write("}\n")
   output_file.flush()
 
+
 def _update_subgraph(full, subgraph, task):
   parents = full.pred[task]
   subgraph.add_node(task)
@@ -223,14 +258,14 @@ def _update_subgraph(full, subgraph, task):
 def _return_non_random_schedules(simulation):
   res = []
   classes = [DLS(simulation), HCPT(simulation), HEFT(simulation),
-             Lookahead(simulation), PEFT(simulation), RoundRobinStatic(simulation)]
+             Lookahead(simulation), PEFT(simulation)]
   for cl in classes:
     res.append(cl.get_schedule(simulation))
-
   return res
 
 
-def _weighted_choice(sum_weights, weights):
+def _weighted_choice(weights):
+  sum_weights = sum(weights)
   r = random.uniform(0, sum_weights)
   upto = 0
   for idx in range(len(weights)):
@@ -240,30 +275,48 @@ def _weighted_choice(sum_weights, weights):
     upto += w
   assert False, "Shouldn't get here"
 
+
+def _make_chromosome_copy(gen):
+  new_gen = Chromosome(gen.tasks, gen.hosts, gen.nxgraph)
+  new_gen.score = gen.score
+  new_gen.scheduling = copy.copy(gen.scheduling)
+  new_gen.matching = copy.copy(gen.matching)
+  return new_gen
+
+
 class GA(StaticScheduler):
   """
   Genetic-Algorithm-Based Approach
 
   """
 
-
   def get_schedule(self, simulation):
     """
     Overridden.
     """
     random.seed(12345)
-    POPULATION_SIZE = 10
-    NUMBER_OF_ITERATION = 2
+    POPULATION_SIZE = 20
+    NUMBER_OF_ITERATION = 10
     RANK_CONSTANT = 1.1
     CROSSOVER_PROBABILITY = 1.0
-
-
+    MUTATION_PROBABILITY = 0.8
 
     nxgraph = simulation.get_task_graph()
     platform_model = cscheduling.PlatformModel(simulation)
     state = cscheduling.SchedulerState(simulation)
 
     topological_order = networkx.topological_sort(nxgraph, reverse=False)
+    topological_order_without_end_and_root = []
+    for task in topological_order:
+      if task.name == "root" or task.name == "end":
+        continue
+      topological_order_without_end_and_root.append(task)
+
+    hosts_without_master = []
+    for host in simulation.hosts:
+      if host.name == "master":
+        continue
+      hosts_without_master.append(host)
 
     ctx = multiprocessing.get_context("spawn")
 
@@ -275,60 +328,68 @@ class GA(StaticScheduler):
 
     popultaion = []
     for schedule in schedules:
-      gen = Chromosome(topological_order, simulation.hosts, nxgraph)
+      gen = Chromosome(topological_order_without_end_and_root, hosts_without_master, nxgraph)
       gen.copy_schedule(schedule)
       popultaion.append(gen)
 
     for i in range(POPULATION_SIZE - len(popultaion)):
-      gen = Chromosome(topological_order, simulation.hosts, nxgraph)
+      gen = Chromosome(topological_order_without_end_and_root, hosts_without_master, nxgraph)
       popultaion.append(gen)
 
-    weights = []
-    for i in range(POPULATION_SIZE):
-      weights.append(RANK_CONSTANT ** (POPULATION_SIZE - i - 1) * (RANK_CONSTANT - 1))
-
-    sum_weights = sum(weights)
-
     for epoh in range(NUMBER_OF_ITERATION):
-      print ("Starting #", epoh, ":")
+      print ("Starting epoh", epoh, ":")
+
+      # Evaluation
       with tempfile.NamedTemporaryFile("w", suffix=".dot") as graph_file:
         _serialize_graph(subgraph, graph_file)
         for gen in popultaion:
-          if epoh == 1:
-            print (gen.get_schedule_by_name())
-            print(gen.scheduling)
-            print(gen.make_task_order())
           with ctx.Pool(1) as process:
             ans = process.apply(_evaluation, (simulation.platform_path, graph_file.name, gen.get_schedule_by_name()))
           gen.score = ans
-          print("ans :", ans)
-
 
       # Selection
+      weights = []
+      population_size = len(popultaion)
+      for i in range(population_size):
+        weights.append(RANK_CONSTANT ** (population_size - i - 1) * (RANK_CONSTANT - 1))
       popultaion = sorted(popultaion, key = lambda t: t.score)
+      print ("Best score", popultaion[0].score)
       new_population = []
       for idx in range(POPULATION_SIZE):
-        new_population.append(copy.copy(popultaion[_weighted_choice(sum_weights, weights)]))
+        index_to_push = _weighted_choice(weights)
+        # print(index_to_push)
+        new_population.append(_make_chromosome_copy(popultaion[index_to_push]))
       popultaion = new_population
 
-      #Crossover
-      new_population = []
+      # Crossover
       for idx in range(POPULATION_SIZE // 2):
-        gen1 = random.choice(popultaion)
-        popultaion.remove(gen1)
-        gen2 = random.choice(popultaion)
-        popultaion.remove(gen2)
+        gen1 = _make_chromosome_copy(random.choice(popultaion))
+        gen2 = _make_chromosome_copy(random.choice(popultaion))
         prob = random.uniform(0, 1)
         if prob < CROSSOVER_PROBABILITY:
           gen1.matching_crossover(gen2)
-          # gen1.scheduling_crossover(gen2)
-        new_population.append(copy.copy(gen1))
-        new_population.append(copy.copy(gen2))
-      print (len(popultaion))
-      popultaion = new_population
+          gen1.scheduling_crossover(gen2)
+          popultaion.append(gen1)
+          popultaion.append(gen2)
 
+      # Mutation
+      for idx in range(POPULATION_SIZE):
+        gen = _make_chromosome_copy(random.choice(popultaion))
+        prob = random.uniform(0, 1)
+        if prob < MUTATION_PROBABILITY:
+          gen.matching_mutation()
+          gen.scheduling_mutation()
+          popultaion.append(gen)
 
-
-
-    return popultaion[0].get_schedule()
+    print("Final population: ")
+    with tempfile.NamedTemporaryFile("w", suffix=".dot") as graph_file:
+      _serialize_graph(subgraph, graph_file)
+      for gen in popultaion:
+        with ctx.Pool(1) as process:
+          ans = process.apply(_evaluation, (simulation.platform_path, graph_file.name, gen.get_schedule_by_name()))
+        gen.score = ans
+        print("Scores:", ans)
+    popultaion = sorted(popultaion, key=lambda t: t.score)
+    (final_schedule, final) = _restore_schedule(simulation, popultaion[0].get_schedule_by_name())
+    return final_schedule, popultaion[0].score
 
